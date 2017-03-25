f75637727212268654568c70304f67615e7b1aba
vishwa <vishwa@riktamtech.com>
2017-03-25 12:03:18 +0530

    db backup

--- module/Application/src/Controller/IndexController.php
+++ module/Application/src/Controller/IndexController.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * @link      http://github.com/zendframework/ZendSkeletonApplication for the canonical source repository
  * @copyright Copyright (c) 2005-2016 Zend Technologies USA Inc. (http://www.zend.com)
@@ -12,8 +13,76 @@ use Zend\View\Model\ViewModel;
 
 class IndexController extends AbstractActionController
 {
+
     public function indexAction()
     {
         return new ViewModel();
     }
+
+    /* To backup the existing db */
+
+    public function backupAction()
+    {
+        $host = 'localhost';
+        $user = 'root';
+        $pass = '123';
+        $name = 'rplace';
+        $tables = '*';
+
+        $link = mysql_connect($host, $user, $pass);
+        if (!$link) {
+            die('Could not connect: ' . mysql_error());
+        }
+        mysql_select_db($name, $link);
+        //get all of the tables
+        if ($tables == '*') {
+            $tables = array();
+            $result = mysql_query('SHOW TABLES');
+            while ($row = mysql_fetch_row($result)) {
+                $tables[] = $row[0];
+            }
+        } else {
+            $tables = is_array($tables) ? $tables : explode(',', $tables);
+        }
+
+        //cycle through
+        $return = 'Backup Time :' . date('d-m-Y H:m:s') . '  (' . time() . ' ) \n\n\n';
+        foreach ($tables as $table) {
+            $result = mysql_query('SELECT * FROM ' . $table);
+            $num_fields = mysql_num_fields($result);
+
+            $return.= 'DROP TABLE ' . $table . ';';
+            $row2 = mysql_fetch_row(mysql_query('SHOW CREATE TABLE ' . $table));
+            $return.= "\n\n" . $row2[1] . ";\n\n";
+
+            for ($i = 0; $i < $num_fields; $i++) {
+                while ($row = mysql_fetch_row($result)) {
+                    $return.= 'INSERT INTO ' . $table . ' VALUES(';
+                    for ($j = 0; $j < $num_fields; $j++) {
+                        $row[$j] = addslashes($row[$j]);
+                        $row[$j] = ereg_replace("\n", "\\n", $row[$j]);
+                        if (isset($row[$j])) {
+                            $return.= '"' . $row[$j] . '"';
+                        } else {
+                            $return.= '""';
+                        }
+                        if ($j < ($num_fields - 1)) {
+                            $return.= ',';
+                        }
+                    }
+                    $return.= ");\n";
+                }
+            }
+            $return.="\n\n\n";
+        }
+        
+        //save file	
+        $filePath = '/var/www/html/Rplace/data_rplace/current-db.sql';
+        $handle = fopen($filePath,'w+');
+        fwrite($handle, $return);
+        fclose($handle);
+
+        return 'success';
+    }
+
 }
